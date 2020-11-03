efd371465e609bc73c574b6034f169b4908760c3
zhouzg <zhouzhengui@gzlplink.com>
2020-11-03 17:38:42 +0800

    初次提交代码

---
+++ gupaoedu-vip-spring-rabbitmq/src/main/java/com/gupaoedu/consumer/FirstConsumer.java
@@ -0,0 +1,17 @@
+package com.gupaoedu.consumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+
+/**
+ * @Author: qingshan
+ * @Description: 咕泡学院，只为更好的你
+ */
+public class FirstConsumer implements MessageListener {
+    private Logger logger = LoggerFactory.getLogger(FirstConsumer.class);
+
+    public void onMessage(Message message) {
+        logger.info("The first consumer received message : " + message.getBody());
+    }
+}
